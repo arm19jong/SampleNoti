fb8d8e65bc003b071584a3e700d0636a76f380f2
suchaj jongprasit <suchaj.j@gmail.com>
2019-01-14 11:50:48 +0700

    add check token

--- app/src/main/java/com/jongzazaal/samplenoti/MainActivity.kt
+++ app/src/main/java/com/jongzazaal/samplenoti/MainActivity.kt
@@ -2,18 +2,20 @@ package com.jongzazaal.samplenoti
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.firebase.iid.FirebaseInstanceId
 import com.google.firebase.messaging.FirebaseMessaging
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
-    var topic: String = "alls"
+    var topic: String = "alls2"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val token = FirebaseInstanceId.getInstance().token
+        Log.i("wqerwr", "Refreshed token:" + token!!)
         sub.setOnClickListener {
             FirebaseMessaging.getInstance().subscribeToTopic(topic)
         }
